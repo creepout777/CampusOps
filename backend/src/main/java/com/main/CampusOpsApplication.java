78ce6209586a2dbe03472bfed2ccbf97b711cb11
creepout777 <yassineaityassine777@gmail.com>
2026-03-04 08:30:23 -0800

    Added backend and frontend folders and files

--- backend/src/main/java/com/main/CampusOpsApplication.java
+++ backend/src/main/java/com/main/CampusOpsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CampusOpsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CampusOpsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CampusOpsApplication.class, args);
+    }
 
 }
